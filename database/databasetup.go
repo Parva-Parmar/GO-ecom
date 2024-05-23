1516fffbe5726f52e7053cc091ddb77410629714
Parva Parmar <parva004cp@gmail.com>
2024-05-24 01:29:48 +0530

    Updated databasetup.go (2.0)

--- database/databasetup.go
+++ database/databasetup.go
@@ -1,9 +1,45 @@
 package database
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+
 func DBSet() *mongo.Client {
 
+		client,err := mongo.NewClient(options.Client().ApplyURI("mongodp://localhost:27017"))
+		if err != nil{
+			log.Fatal(err)
+		}
+
+		ctx , cancel := context.WithTimeOut(context.Background() , 10*time.Second)
+		defer cancel()
+
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		client.Ping(context.TODO(),nil)
+		if err != nil{
+			log.Fatal("Failed to connect to mongodb")
+			return nil
+		}
+
+		fmt.Println("Succesfully connect to mongodb")
+
+		return client
 }
 
+		var Client *mongo.Client = DBSet()
+		
 func Userdata(client *mongo.Client, collectionName string) *mongo.Collection {
 
 }
@@ -11,3 +47,4 @@ func Userdata(client *mongo.Client, collectionName string) *mongo.Collection {
 func ProductData(client *mongo.Client, collectionName string) *mongo.Clooection {
 
 }
+ 
