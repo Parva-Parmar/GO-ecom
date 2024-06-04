1b0fdb85dc1838c91ac6eafb5675149cfe512dc2
Parva Parmar <parva004cp@gmail.com>
2024-06-04 19:27:04 +0530

    updated cart.go files

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,25 +1,71 @@
 package controllers
 
-import(
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
-) 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-func AddToCart() gin.Handler{
+type Application struct {
+	prodCollection *mongo.Collection
+	userCollection *mongo.Collection
+}
+
+func NewApplication(prodCollection, userCollection *mongo.Collection) *Application {
+	return &Application{
+		prodCollection: prodCollection,
+		userCollection: userCollection,
+	}
+}
+
+func (app *Application) AddToCart() gin.Handler {
+	return func(c *gin.Context) {
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("product id is empty")
+
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+		}
+
+		userQueryID := c.Query("userID")
+		if userQueryID == "" {
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+			return
+		}
+
+		productID, err := primitive.ObjectIDFromHex(productQueryID)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeOut(context.Background(), 5*time.Second)
+
+		defer cancel()
+
+	}
+}
+
+func RemoteItem() gin.HandlerFunc {
 
 }
 
-func RemoteItem() gin.HandlerFunc{
+func GetItemFromCart() gin.HandlerFunc {
 
 }
 
-func GetItemFromCart() gin.HandlerFunc{
+func BuyFromCart() gin.HandlerFunc {
 
 }
 
-func BuyFromCart() gin.HandlerFunc{
+func InstantBuy() gin.HandlerFunc {
 
-}
-
-func InstantBuy() gin.HandlerFunc{
-	
 }
