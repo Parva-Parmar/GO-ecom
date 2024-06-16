c043fb2839e7529ae53eb4d551d4d2101d839e74
Parva Parmar <parva004cp@gmail.com>
2024-06-16 18:34:55 +0530

    Update address.go

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,7 @@
 package controllers
 
-import(
+import (
+	"github.com/Parva-Parmar/GO-ecom/models"
 	"github.com/gin-gonic/gin"
 ) 
 
@@ -18,5 +19,20 @@ func EditWorkAddress() gin.HandlerFunc{
 }
 
 func DeleteAddress() gin.HandlerFunc{
-	
+	return func(c *gin.Context){
+		user_id := c.Query("id")
+
+		if user_id == ""{
+			c.Header("Content-Type","application/json")
+			c.JSON(http.StatusNotFound,gin.H{"Error":"Invalid Search Index"})
+			c.Abort()
+			return 
+		}
+
+		addresses := make([]models.Address,0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil{
+			c.IndentedJSON(500,"Internal Server Error")
+		} 
+	}
 }
