b794d0ae2a88c7e0a4b36634b334b47644c72095
Parva Parmar <parva004cp@gmail.com>
2024-06-16 18:57:38 +0530

    Update address.go (DeleteAddress Function)

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"context"
+	"time"
+
 	"github.com/Parva-Parmar/GO-ecom/models"
 	"github.com/gin-gonic/gin"
 ) 
@@ -34,5 +37,18 @@ func DeleteAddress() gin.HandlerFunc{
 		if err != nil{
 			c.IndentedJSON(500,"Internal Server Error")
 		} 
+
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{Key:"_id",Value: usert_id}}
+		update := bson.D{{Key:"$set",Value: bson.D{primitive.E{Key:"address",Value: addresses}}}}
+		_,err = UserCollection.UpdateOne(ctx,filter,update)
+		if err != nil{
+			c.IndentedJSON(404,"Wrong command")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200,"Succesfully Deleted")
 	}
 }
