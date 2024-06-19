df3ea713cea18a2230a6354eb105fe0a3b2e2f16
Parva Parmar <parva004cp@gmail.com>
2024-06-19 20:41:30 +0530

    Update database.cart.go

--- database/cart.go
+++ database/cart.go
@@ -1,7 +1,12 @@
 package database
 
-import(
+import (
+	"context"
+	"log"
 
+	"github.com/Parva-Parmar/GO-ecom/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
@@ -16,8 +21,32 @@ var (
 
 )
 
-func AddProductToCart(){
+func AddProductToCart(ctx context.Context,prodCollection,userCollection *mongo.Collection,productID primitive.ObjectID,userID string) error{
+	searchfromdb,err := prodCollection.Find(ctx,bson.m{"_id":productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchfromdb.All(ctx,&productCart)
+	if err != nil{
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+	id,err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIdNotValid
+	}
 
+	filter := bson.D{primitive.E{Key:"_id",Value:id}}
+	update := bson.D{{Key:"$push",Value: bson.D{primitive.E{Key:"usercart",Value: bson.D{{Key:"$each",Value:productCart}}}}}}
+
+	_,err = userCollection.UpdateOne(ctx,fliter,update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
 }
 
 func RemoveCartItem(){
